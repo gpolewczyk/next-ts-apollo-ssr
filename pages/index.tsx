3205d9ece19f5b36a0c90f5912ba481f19b46cda
Gniewomir Polewczyk <gniewomir.polewczyk@iteo.com>
2020-09-02 14:28:42 +0200

    Configure apollo client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,26 @@
-const Home = () => <div className="home">Hello world!</div>;
+import { gql, useQuery } from "@apollo/client";
+
+const GET_COUNTRIES = gql`
+  query {
+    Country {
+      _id
+      name
+    }
+  }
+`;
+
+const Home = () => {
+  const { data } = useQuery(GET_COUNTRIES);
+
+  if (!data) return null;
+
+  return (
+    <ul className="countries">
+      {data.Country.map((country) => (
+        <li key={country._id}>{country.name}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default Home;
